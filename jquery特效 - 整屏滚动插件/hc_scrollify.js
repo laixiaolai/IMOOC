8c7c9e6cfca87bc1b3514692727b22f31be546bc
laixiaolai <laiyongzhao@gmail.com>
2014-08-11 11:12:53 +0800

    imooc

---
+++ jquery特效 - 整屏滚动插件/hc_scrollify.js
@@ -0,0 +1,42 @@
+﻿//整屏滚动函数
+function hc_scrollify(class_name)
+{
+    var set_time_id = null;
+    var win_heigth = $(window).height();
+    $('.'+class_name).css({height: win_heigth});
+    $('<input type="hidden" value="0" id="sc_top_hidden_no_rename" /><input type="hidden" value="2" id="sc_is_hidden_no_rename" />').appendTo('body');
+    $(window).scroll(function()
+    {
+        var sc_top    = $(window).scrollTop();
+        if(sc_top == $('#sc_top_hidden_no_rename').val()) return false;
+        if(sc_top <= 0) return false;
+        if($('#sc_is_hidden_no_rename').val() == 3) return false;
+        if(set_time_id != null) clearTimeout(set_time_id);
+        $('html,body').stop();
+        set_time_id = setTimeout(function()
+        {
+            var number    = parseInt(sc_top / win_heigth);
+            var old_sctop = Number($('#sc_top_hidden_no_rename').val());
+            if(old_sctop == 0)
+            {
+                number += 1;
+            }
+            else
+            {
+                if(sc_top > old_sctop)
+                {
+                    number += 1;
+                }
+            }
+            if(number < 0) number = 0;
+            var sc_top_val = number * win_heigth;
+            $('#sc_is_hidden_no_rename').val(3);
+            $('#sc_top_hidden_no_rename').val(sc_top_val);
+            $('html,body').animate(
+            {
+                'scrollTop' : sc_top_val
+            },800,function(){$('#sc_is_hidden_no_rename').val(2);});
+            $('#sc_top_hidden_no_rename').val(sc_top_val);
+        },100);
+    });
+}
---
+++ mobile-gallery-master/README.md
@@ -0,0 +1,17 @@
+##mobile-gallery
+* 版本：1.0
+* [在线视频传送门](http://www.imooc.com/learn/140)
+* demo: [http://xuyuan923.github.io/demo/mobile-gallery.html](http://xuyuan923.github.io/demo/mobile-gallery.html)
+* 原作者: @远人
+* 整理&简单修改by：徐媛
+* 博客教程：[http://xuyuan923.github.io/2014/06/30/mobile-gallery/](http://xuyuan923.github.io/2014/06/30/mobile-gallery/)
+
+##所用工具(js库,css库)
+* js：[zepto.js](https://github.com/madrobby/zepto)
+* css3: [animate.css](http://daneden.github.io/animate.css/)
+
+##功能
+* 图片列表4张图片一排分布
+* 触摸小图出现大图
+* 实现大图左滑、右滑翻页
+
